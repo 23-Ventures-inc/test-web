5560c73dee0544033fbcb82919310b141a3dc820
anjalikamalwa <kamalwaanjali@gmail.com>
2025-01-05 23:37:10 +0530

    work on text

--- components/ideas.tsx
+++ components/ideas.tsx
@@ -11,11 +11,11 @@ const Ideas = () => {
     return (
         <div>
             <div className="w-full my-5 flex justify-center items-center flex-col py-5 text-center h-[600px] relative z-50 bg-[#030908]">
-                <h1 className='text-5xl py-2 text-white'>We build people < br />
+                <h1 className='text-5xl py-2 text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-500 my-2'>We build people < br />
 
                     beyond ideas</h1>
 
-                <p className='text-xl font-medium py-2 text-white'>This is not just about <br />
+                <p className='text-xl font-medium py-2 text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-500'>This is not just about <br />
 
                     backing ideas, its about backing people !!</p>
                 <div className=' flex w-full h-full -z-50 absolute py-2'>
